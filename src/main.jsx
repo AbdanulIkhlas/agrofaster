7d84ff801aa5459161a14cc55ae38ed278f83736
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-05-15 00:34:56 +0700

    configuration tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import Beranda from "./pages/beranda";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Beranda />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/beranda.jsx
@@ -0,0 +1,9 @@
+const Beranda = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-white bg-purple-600">Beranda</h1>
+    </div>
+  );
+};
+
+export default Beranda;
