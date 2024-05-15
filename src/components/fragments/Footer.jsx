9b1d783bf66ebaf87c334f07773509bd071b6e98
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-05-15 11:06:05 +0700

    membuat footer

---
+++ src/components/fragments/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return <div>
+		<footer className="w-full font-jakartaSans bg-slate-500 py-10">
+			<p className="text-center text-white bg-purple-600 text-sm py-4">Copyright © 2022. All Rights Reserved.</p>
+		</footer>
+	</div>;
+};
+
+export default Footer;
