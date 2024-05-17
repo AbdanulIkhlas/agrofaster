78e89ab3aaa2700948261c903b10bfc7b181e01b
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-05-17 14:20:13 +0700

    membuat section testimoni

---
+++ src/components/layouts/BerandaTestimoni.jsx
@@ -0,0 +1,21 @@
+import Caraousel from "../fragments/Caraousel";
+import {testimoni} from "../../data/testimoni";
+const BerandaTestimoni = () => {
+  return (
+    <div>
+      <h1 className="text-center font-black text-2xl lg:text-4xl text-[#2C946C] ">
+        Testimoni
+      </h1>
+      <section className="mt-6">
+        <Caraousel
+          data={testimoni}
+          srcLeftButtonPath={`../../svg/green-left-arrow.svg`}
+          srcRightButtonPath={`../../svg/green-right-arrow.svg`}
+          chooseFragment="testimoni"
+        />
+      </section>
+    </div>
+  );
+};
+
+export default BerandaTestimoni;
---
+++ src/data/testimoni.js
@@ -0,0 +1,18 @@
+export const testimoni = [
+  {
+    id: 1,
+    image: "../images/testimoni-1.png",
+  },
+  {
+    id: 1,
+    image: "../images/testimoni-2.png",
+  },
+  {
+    id: 1,
+    image: "../images/testimoni-3.png",
+  },
+  {
+    id: 1,
+    image: "../images/testimoni-4.png",
+  },
+];
