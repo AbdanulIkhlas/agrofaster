fe1349d3c48bf05a83b94607ace05dd9bc7c803e
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-05-17 10:19:56 +0700

    konfigurasi box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ export default {
         cardShadow: "0 5px 16px 0 rgba(0, 0, 0, 0.15)",
         buttonShadow: "0 4px 4px 0 rgba(0, 0, 0, 0.25)",
         secondCardShadow:
-          "box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;",
+          "rgba(0, 0, 0, 0.25) 0px 0.1em 0.1em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;",
       },
       borderRadius: {
         customRounded: "52% 48% 48% 52% / 50% 54% 46% 50% ",
