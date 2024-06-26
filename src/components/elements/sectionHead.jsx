740691bf50c01dc39913aa7509514b35fbb4eef4
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-06-26 14:22:25 +0700

    membuat komponen section head

---
+++ src/components/elements/sectionHead.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+const SectionHead = (props) => {
+  const { content = [], subsec = false, customClassName } = props;
+
+  return (
+    <h1
+      className={`text-black font-jakartaSans lg:text-[34px] ${customClassName}`}
+    >
+      {content.map((item, index) => (
+        <span
+          key={index}
+          className={`${item.classname} ${subsec ? "text-lg" : "text-2xl"}`}
+        >
+          {item.title}
+        </span>
+      ))}
+    </h1>
+  );
+};
+
+SectionHead.propTypes = {
+  content: PropTypes.array.isRequired,
+  subsec: PropTypes.bool,
+};
+
+export default SectionHead;
