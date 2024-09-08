fadfc748f62bcb7797a008220ec2397fc14b7ac1
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-09-08 21:27:08 +0700

    menambahkan image untuk keperluan website

--- src/pages/beliHasilPanen.jsx
+++ src/pages/beliHasilPanen.jsx
@@ -1,16 +1,12 @@
-import UnderDevelopment from "../components/fragments/UnderDevelopment";
-import Navbar from "../components/fragments/Navbar";
-import Footer from "../components/fragments/Footer";
+// import UnderDevelopment from "../components/fragments/UnderDevelopment";
+// import Navbar from "../components/fragments/Navbar";
+// import Footer from "../components/fragments/Footer";
+import SplashScreen from "../components/fragments/SplashScreen";
 
 const BeliHasilPanen = () => {
   return (
     <div className="w-full h-screen flex flex-col ">
-      <Navbar />
-      <main className="flex flex-col items-center min-h-[800px] bg-shadowGreenn">
-        <UnderDevelopment />
-        <section className="w-[140px] mt-14"></section>
-      </main>
-      <Footer />
+      <SplashScreen />
     </div>
   );
 };
