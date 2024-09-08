f3ad8cb1f3458eedd7f5bd1b5677adccbab9f366
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-09-08 21:58:04 +0700

    membuat splash screen

--- src/components/fragments/SplashScreen.jsx
+++ src/components/fragments/SplashScreen.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "../../assets/css/SplashScreen.css"; // Import file CSS yang berisi keyframes dan animasi tambahan
 
-const SplashScreen = () => {
+const SplashScreen = ({ onFinish }) => {
+  const [showFirstImage, setShowFirstImage] = useState(false);
+  const [showSecondImage, setShowSecondImage] = useState(false);
+
+  useEffect(() => {
+    const runAnimation = async () => {
+      setShowFirstImage(true);
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // delay 2 detik untuk logo pertama
+      setShowSecondImage(true);
+      await new Promise((resolve) => setTimeout(resolve, 3000)); // delay 3 detik sebelum splash hilang
+      onFinish(); // Mengakhiri splash screen dan menampilkan main
+    };
+
+    runAnimation();
+  }, [onFinish]);
+
   return (
-    <div>
-        
+    <div className="absolute z-50 w-full h-full flex items-center justify-center bg-edgewater">
+      <div className="flex flex-col items-center p-4">
+        {showFirstImage && (
+          <img
+            src="../../../public/images/agrofasterLogoSplash.png"
+            alt="Logo"
+            className="w-[200px] -mt-10 transition-opacity duration-700 ease-in-out opacity-100"
+          />
+        )}
+        {showSecondImage && (
+          <img
+            src="../../../public/images/agrofasterTextSplash.png"
+            alt="Text"
+            className="w-[200px] -mt-5 fade-in-bounce" // Menambahkan kelas untuk animasi bounce dan fade-in
+          />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
--- src/pages/beliHasilPanen.jsx
+++ src/pages/beliHasilPanen.jsx
@@ -1,12 +1,24 @@
-// import UnderDevelopment from "../components/fragments/UnderDevelopment";
-// import Navbar from "../components/fragments/Navbar";
-// import Footer from "../components/fragments/Footer";
+import React, { useState } from "react";
 import SplashScreen from "../components/fragments/SplashScreen";
 
 const BeliHasilPanen = () => {
+  const [showSplash, setShowSplash] = useState(true);
+
+  const handleSplashFinish = () => {
+    setShowSplash(false); // Menghilangkan splash screen setelah animasi selesai
+  };
+
   return (
-    <div className="w-full h-screen flex flex-col ">
-      <SplashScreen />
+    <div className="relative w-full h-screen flex flex-col">
+      {showSplash && <SplashScreen onFinish={handleSplashFinish} />}
+      <main
+        className={`transition-opacity duration-1000 ${
+          showSplash ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        <h1>Selamat Datang di Halaman Beli Hasil Panen</h1>
+        {/* Konten lainnya */}
+      </main>
     </div>
   );
 };
