254d7f4d2e66d4df99589fe82aeda88e2cf176b9
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-09-10 00:22:53 +0700

    membuat page detail dan kategory saprodi

--- src/components/fragments/KategoryCard.jsx
+++ src/components/fragments/KategoryCard.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const KategoryCard = ({ id, image, kategori }) => {
+const KategoryCard = ({ id, image, kategori, page }) => {
+  const linkTo =
+    page === "saprodi"
+      ? `/produk/fast-tani/saprodi/kategori-saprodi/${kategori}`
+      : page === "konsumen"
+      ? `/produk/konsumen/beli-hasil-panen/kategori/${kategori}`
+      : null;
+
   return (
-    <Link to={`kategori/${kategori}`}>
+    <Link to={`${linkTo}`}>
       <div className="flex gap-4 py-3 px-2 w-max font-jakartaSans items-center rounded-[10px] bg-[#2c946c5b]">
         <h1 className="text-lg">{kategori}</h1>
         <img
           src={image}
           alt="image"
-          className="w-[45px] rounded border border-gray-400"
+          className="w-[45px] rounded border border-gray-400 bg-white"
         />
       </div>
     </Link>
